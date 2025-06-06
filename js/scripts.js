be8b28b0e2e700ee49f6f01dec72184337863f6b
KayoCosta <kaayo.costa@hotmail.com>
2025-06-06 15:12:42 -0300

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+const dayEl = document.getElementById("day");
+
+
+// Atualizar relógio e data
+function atualizarTempo() {
+  const now = new Date();
+  timeEl.textContent = now.toLocaleTimeString("pt-BR");
+  dateEl.textContent = now.toLocaleDateString("pt-BR", {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  });
+  dayEl.textContent = now.toLocaleDateString("pt-BR", { weekday: 'long' });
+}
+
+setInterval(atualizarTempo, 1000);
+
+atualizarTempo();
